af6829298073d15b9e46c1aa835f76e462b9df60
cailing12 <caiyi168@qq.com>
2020-05-23 19:15:43 +0800

    user

--- Wuther.Api/Controllers/LoginController.cs
+++ Wuther.Api/Controllers/LoginController.cs
@@ -8,36 +8,34 @@ using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Logging;
 using Wuther.Entities.Models;
 
-namespace Wuther.Api.Controllers
-{
+namespace Wuther.Api.Controllers {
     [ApiController]
-    [Route("[controller]")]
-    public class LoginController : ControllerBase
-    {
-      DbContext context;
-      public LoginController(DbContext context)
-      {
-          this.context = context;
-      }
+    [Route ("[controller]")]
+    public class LoginController : ControllerBase {
+        DbContext context;
+        public LoginController (DbContext context) {
+            this.context = context;
+        }
 
         [HttpPost]
-        public IActionResult Post(Users user)
-        {
-            Console.WriteLine("3333");
-            var list = context.Set<Users>().Where(c=>c.Account == user.Username && c.Password == user.Password);
-            if(list.Any()){
-                return new JsonResult(new { code = 200, msg = "成功", token = user.Username + user.Password }); 
+        public IActionResult Post (Users user) {
+            var list = context.Set<Users> ().Where (c => c.Account == user.Username && c.Password == user.Password);
+            if (list.Any ()) {
+                return new JsonResult (new { code = 200, msg = "成功", token = user.Username + user.Password });
+            } else {
+                return new JsonResult (new { code = -1, msg = "失败" });
             }
-            else
-            {
-                return new JsonResult(new { code = -1, msg = "失败" });
-            }
-            
+
         }
 
         [HttpGet]
-        public IActionResult Get(){
-            return new JsonResult("AAAAAAAAA");
+        public IActionResult Get () {
+            return new JsonResult ("AAAAAAAAA");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete () {
+            return new JsonResult ("BBBBBB");
         }
     }
 }
